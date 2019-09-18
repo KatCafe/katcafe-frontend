6503e0da30f0092308f3f696280b3586c0c5a818
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-09-18 22:35:38 +0300

    fix infinite scroll

--- consts/consts.js
+++ consts/consts.js
@@ -3,9 +3,9 @@ module.exports = {
     port: 8089,
     serverApi: "http://registratura.ddns.net:8085",
 
-    name: "catcafé",
-    copyright: "catcafé",
-    title: "catcafé - Your café",
+    name: "katcafé",
+    copyright: "katcafé",
+    title: "katcafé - Your café",
     description: "Discuss your stuff here",
     keywords: "cafe, 4chan, reddit",
     image: "/public/assets/",
--- src/client/components/UI/elements/infinite-scroll.vue
+++ src/client/components/UI/elements/infinite-scroll.vue
@@ -2,21 +2,7 @@
 
     <div ref="refInfinite"  class="infiniteScroll">
 
-        <!-- 2 -->
-        <div v-if="loading" class="loader loader--style2" title="1">
-            <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                 width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
-                <path fill="#000" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z">
-                <animateTransform attributeType="xml"
-                                  attributeName="transform"
-                                  type="rotate"
-                                  from="0 25 25"
-                                  to="360 25 25"
-                                  dur="0.6s"
-                                  repeatCount="indefinite"/>
-                </path>
-            </svg>
-        </div>
+        <icon v-if="loading" icon="loading-spinner" class="fa-5x" />
 
         <br/>
 
@@ -31,7 +17,12 @@
 
 
 <script>
+
+import Icon from "client/components/UI/elements/icons/icon"
+
 export default{
+
+    components: { Icon },
 
     mounted(){
 
