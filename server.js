6d4f32a90a6a84c7a31967992dd9bf8cf597d1fd
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-09-23 22:23:21 +0300

    fix port

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ app.get('*', isProd ? render : (req, res) => {
     readyPromise.then(() => render(req, res))
 })
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || ( process.env.NODE_ENV === 'production' ? 80 : consts.port);
 
 app.listen(port, () => {
     console.log(`server started at localhost:${port}`)
