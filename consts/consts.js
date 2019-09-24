f94f1a6f243895d243ff7a22b88865e7530d0846
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-09-24 16:59:44 +0300

    fix logo

--- consts/consts.js
+++ consts/consts.js
@@ -1,15 +1,14 @@
 module.exports = {
 
     port: 8080,
-    serverApi: "http://registratura.ddns.net:8085",
-    //serverApi: "https://katcafe.org:2095",
+    serverApi: "https://katcafe.org:2096",
 
     name: "katcafé",
     copyright: "katcafé",
     title: "katcafé - Your café to discuss anything",
     description: "Discuss anything from politics to crypto or life",
     keywords: "cafe, 4chan, reddit, discussions, freedom of speech, politics, religions, games, memes",
-    image: "katcafe.org/public/assets/katcafe-cover.jpg",
-    logo: 'katcafe.org/public/assets/katcafe-logo.png',
+    image: "https://katcafe.org/public/assets/katcafe-cover.jpg",
+    logo: 'https://katcafe.org/public/assets/katcafe-logo.png',
 
 }
