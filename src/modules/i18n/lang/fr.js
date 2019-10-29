a7133df83071d589a19a65f642527d55cae624c3
Popol <specialpopol@gmx.fr>
2019-10-29 13:15:33 +0100

    Create fr.js

---
+++ src/modules/i18n/lang/fr.js
@@ -0,0 +1,51 @@
+export default {
+
+    plural: {
+        comment: 'pas de commentaires | un commentaire | {n} commentaires'
+    },
+
+    topic: {
+        startThread: "Créer un sujet dans",
+        viewMoreComments: "montrer plus",
+        threadTitle: "Titre du nouveau sujet",
+        threadComment: "Votre commentaire...",
+        previewThread: "Prévisualiser le sujet",
+        submitTopic: 'Publier',
+        postAnonymously: 'Poster anonymement',
+        errorTooFewLetters: "Trop court. 4 lettres minimum.",
+    },
+
+    comment: {
+        anonymous: 'Anonyme',
+        yourOpinionMatters: "Votre opinion compte",
+        writeReply: 'écrire une réponse...',
+        previewComment: "Prévisualisation du commentaire",
+        submitComment: "Publier",
+        errorNoFileOrText: "Vous devez entrer un lien ou un fichier ou écrire au moins 5 caractères",
+    },
+
+    captcha:{
+        captcha: 'captcha',
+        post: 'envoyer',
+        modalText: 'Pour des raisons de sécurité, veuillez entrer le code captcha',
+        errorIncorrect: 'La captcha est incorrect',
+        errorAlreadyUsed: 'Le captcha a déjà été utilisé',
+    },
+
+    header: {
+        addChannel: 'Ajouter un forum',
+        login: 'Se connecter',
+        signup: 'S\'enregistrer',
+        logout: 'Se déconnecter',
+        admin: 'admin',
+    },
+
+    socialMedia:{
+        text: 'Partager',
+    },
+
+    notifications:{
+        askPermission: "Souaitez-vous recevoir une notification lorsque des personnes répondent à vos messages ?"
+    },
+
+}
